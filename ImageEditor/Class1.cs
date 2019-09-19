3e60949b7f0ca3213cd83a528bf930cd253af866
Martin <martin.goransson94@gmail.com>
2019-09-19 12:19:57 +0200

    Initital commit

---
+++ ImageEditor/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace ImageEditor
+{
+    public class ImageEditingProgram
+    {
+        public static Bitmap MakeImageGreyScale(Bitmap originalImage)
+        {
+            //Bitmap originalImage = new Bitmap(imageFilePath);
+            Bitmap greyScaleImage = new Bitmap(originalImage.Width,originalImage.Height);
+            //pixel value = medelvärde alla pixlar
+
+            for (int x = 0; x < originalImage.Width; x++) {
+                for (int y = 0; y < originalImage.Height    ; y++)
+                {
+                    Color pixels = originalImage.GetPixel(x,y);
+                    int newPixelValue = (pixels.R + pixels.G + pixels.B) / 3;
+                    greyScaleImage.SetPixel(x,y,Color.FromArgb(255,newPixelValue,newPixelValue,newPixelValue));
+                }
+
+            }
+
+            return greyScaleImage;
+        }
+
+        public static Bitmap MakeImageNegative(Bitmap originalImage)
+        {
+
+            //pixel value = 255-current pixelvalue
+            //Bitmap originalImage = new Bitmap(imageFilePath);
+            Bitmap negativeImage = new Bitmap(originalImage.Width, originalImage.Height);
+
+            for (int x = 0; x < originalImage.Width; x++)
+            {
+                for (int y = 0; y < originalImage.Height; y++)
+                {
+                    Color pixels = originalImage.GetPixel(x, y);
+                    int newRValue = 255-pixels.R;
+                    int newGValue = 255 - pixels.G;
+                    int newBValue = 255 - pixels.B;
+                    negativeImage.SetPixel(x, y, Color.FromArgb(255, newRValue, newGValue, newBValue));
+
+                }
+
+            }
+
+            return negativeImage;
+        }
+
+        public static Bitmap MakeImageBlurred(Bitmap originalImage)
+        {
+            Bitmap blurred = new Bitmap(originalImage.Width, originalImage.Height);
+
+            for (int x = 0; x < originalImage.Width; x++)
+            {
+                for (int y = 0; y < originalImage.Height; y++)
+                {
+                    
+
+                }
+
+            }
+
+            return blurred;
+        }
+    }
+}
---
+++ ImageEditorConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ImageEditor;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ImageEditorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string imagePath = args[0];
+
+            // try catch för args[0]!null och filepath = correct
+            if (!String.IsNullOrEmpty(args[0])) {
+                Bitmap originalImage = new Bitmap(imagePath);
+                Bitmap greyScale = ImageEditingProgram.MakeImageGreyScale(originalImage);
+                Bitmap negative = ImageEditingProgram.MakeImageNegative(originalImage);
+                Bitmap blurred = ImageEditingProgram.MakeImageBlurred(originalImage);
+                //new file = old.replace @. with _greyscale.jpg
+
+                string storageDirectory = Path.GetDirectoryName(imagePath);
+                string fileName = Path.GetFileNameWithoutExtension(imagePath);
+
+                greyScale.Save(storageDirectory + "\\"+ fileName +  "_greyScale.jpg", ImageFormat.Jpeg);
+                negative.Save(storageDirectory + "\\" + fileName + "_negative.jpg", ImageFormat.Jpeg);
+            }
+
+
+            else
+            {
+                Console.WriteLine("Type correct filepath for the image");
+                imagePath = Console.ReadLine();
+            }
+
+        }
+    }
+}
