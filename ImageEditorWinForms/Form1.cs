4e2d60bb35b2cf959e08438eebb17a5c38775f62
Martin <martin.goransson94@gmail.com>
2019-09-20 09:06:35 +0200

    Main functionality complete

--- ImageEditorWinForms/Form1.cs
+++ ImageEditorWinForms/Form1.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,32 +18,43 @@ namespace ImageEditorWinForms
         public Form1()
         {
             InitializeComponent();
+
+            pictureBox1.Height = 250;
+            pictureBox1.Width = 150;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+            pictureBox2.Height = 250;
+            pictureBox2.Width = 150;
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
         }
-        string imagePath;
+        string imagePath="";
+        
+        
         private void Button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
             openFileDialog1.InitialDirectory = "C:\\";
-            openFileDialog1.Filter = "Image Files (*.PNG; *.JPG)| *.PNG; *.JPG";    
-            if (openFileDialog1.ShowDialog()==DialogResult.OK)
+            openFileDialog1.Filter = "Image Files (*.PNG; *.JPG)| *.PNG; *.JPG";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 imagePath = openFileDialog1.FileName;
                 pictureBox1.Image = Image.FromFile(imagePath);
-                pictureBox1.Height = 250;
-                pictureBox1.Width = 150;
-                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
             }
         }
-
+        
         private void Button2_Click(object sender, EventArgs e)
         {
             Bitmap originalImage = new Bitmap(imagePath);
             Bitmap greyScale = ImageEditingProgram.MakeImageGreyScale(originalImage);
 
             pictureBox2.Image = greyScale;
-            pictureBox2.Height = 250;
-            pictureBox2.Width = 150;
-            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
         }
 
         private void Button3_Click(object sender, EventArgs e)
@@ -50,9 +63,6 @@ namespace ImageEditorWinForms
             Bitmap negative = ImageEditingProgram.MakeImageNegative(originalImage);
 
             pictureBox2.Image = negative;
-            pictureBox2.Height = 250;
-            pictureBox2.Width = 150;
-            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
         }
 
         private void Button4_Click(object sender, EventArgs e)
@@ -61,14 +71,19 @@ namespace ImageEditorWinForms
             Bitmap blurred = ImageEditingProgram.MakeImageBlurred(originalImage);
 
             pictureBox2.Image = blurred;
-            pictureBox2.Height = 250;
-            pictureBox2.Width = 150;
-            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
         }
 
         private void Button5_Click(object sender, EventArgs e)
         {
+            saveFileDialog1.InitialDirectory = "C:\\";
+            saveFileDialog1.Filter = "Image Files (*.JPG)| *.JPG";
 
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(imagePath);
+                pictureBox2.Image.Save(saveFileDialog1.FileName);
+            }
+            
         }
     }
 }
